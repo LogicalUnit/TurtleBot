96d851da6696508205333b0dfe4f6edc19d351ba
John Crain Welsby <john.welsby@gmail.com>
2017-04-15 17:29:06 +1000

    beautified image_filtering.cpp

--- image_filtering.cpp
+++ image_filtering.cpp
@@ -8,63 +8,63 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	unsigned char values[] = { 0, 1, 1, 0, 0, 0, 1, 1 };
+    unsigned char values[] = { 0, 1, 1, 0, 0, 0, 1, 1 };
 
-	std::vector<unsigned char> test(values, values+sizeof(values));			
-	
-	for(int i = 0; i < test.size(); i++)
-	{
-		std::cout<<(unsigned int)test[i]<<", ";
-	}
-	
-	std::cout<<std::endl;
-	
-	std::vector<unsigned char> dilated = dilateLine(test);
-	
-	for(int i = 0; i < dilated.size(); i++)
-	{
-		std::cout<<(unsigned int)dilated[i]<<", ";
-	}
-	
-	std::cout<<std::endl;
-	
-	std::vector<unsigned char> eroded = erodeLine(dilated);
-	
-	for(int i = 0; i < eroded.size(); i++)
-	{
-		std::cout<<(unsigned int)eroded[i]<<", ";	
-	}
+    std::vector<unsigned char> test(values, values+sizeof(values));			
 
-	std::cout<<std::endl;	
-		
-	unsigned int centroid = findBestCentroid(eroded);
-	
-	std::cout<<"Best Centroid: "<<centroid<<std::endl;
+    for(int i = 0; i < test.size(); i++)
+    {
+        std::cout<<(unsigned int)test[i]<<", ";
+    }
+
+    std::cout<<std::endl;
+
+    std::vector<unsigned char> dilated = dilateLine(test);
+
+    for(int i = 0; i < dilated.size(); i++)
+    {
+        std::cout<<(unsigned int)dilated[i]<<", ";
+    }
+
+    std::cout<<std::endl;
+
+    std::vector<unsigned char> eroded = erodeLine(dilated);
+
+    for(int i = 0; i < eroded.size(); i++)
+    {
+        std::cout<<(unsigned int)eroded[i]<<", ";	
+    }
+
+    std::cout<<std::endl;	
+
+    unsigned int centroid = findBestCentroid(eroded);
+
+    std::cout<<"Best Centroid: "<<centroid<<std::endl;
 
 
 /*
-	Mat source = imread("/home/turtlebot/coloured_squares.png");
-	
-	imshow("source", source);
-	waitKey();
-	
-	Mat redOnly = redFilter(source);
-	
-	imshow("Red Only", redOnly);
-	waitKey();
-	
-	Mat blueOnly = blueFilter(source);
-	
-	imshow("Blue Only", blueOnly);
-	waitKey();
-	
-	Mat greenOnly = greenFilter(source);
+    Mat source = imread("/home/turtlebot/coloured_squares.png");
 
-	imshow("Green Only", greenOnly);
-	waitKey();
+    imshow("source", source);
+    waitKey();
+
+    Mat redOnly = redFilter(source);
+
+    imshow("Red Only", redOnly);
+    waitKey();
+
+    Mat blueOnly = blueFilter(source);
+
+    imshow("Blue Only", blueOnly);
+    waitKey();
+
+    Mat greenOnly = greenFilter(source);
+
+    imshow("Green Only", greenOnly);
+    waitKey();
 */
 	
-	return 0;
+    return 0;
 
 }
 
