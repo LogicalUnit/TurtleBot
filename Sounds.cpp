e3fda5ab6570d1588fd80af043a456a413e6d58c
John Crain Welsby <john.welsby@gmail.com>
2017-04-15 17:33:52 +1000

    beautified Sounds.cpp

--- Sounds.cpp
+++ Sounds.cpp
@@ -5,9 +5,9 @@
 
 void playSound(const std::string& filepath)
 {
-	sound_play::SoundClient sc;
-	
-	sc.playWave(filepath);
-	
-	boost::this_thread::sleep(boost::posix_time::milliseconds(5000));
+    sound_play::SoundClient sc;
+
+    sc.playWave(filepath);
+
+    boost::this_thread::sleep(boost::posix_time::milliseconds(5000));
 }
